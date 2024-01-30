9b17418838129dd49f986cc2bfab752871f7f91b
ara6oss <96424687+ara6oss@users.noreply.github.com>
2024-01-30 21:13:03 +0600

    Added goods app, connected catalog & product html

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
+    
 ]
