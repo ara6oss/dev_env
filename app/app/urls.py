1e6d43fb7695fb0186032496bebb43dff1cd47f4
ara6oss <96424687+ara6oss@users.noreply.github.com>
2024-02-01 23:27:51 +0600

    after adding fixtures for goods

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,8 @@
+from argparse import Namespace
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='index'),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog'))
     
